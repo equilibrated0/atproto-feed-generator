050ca771feccbd543ba2ed08fa51e5b368b8da20
Kramer Campbell <kramer@kramerc.com>
2025-12-15 16:26:26 -0800

    Add happy hardcore feed

--- src/subscription.ts
+++ src/subscription.ts
@@ -9,22 +9,14 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     if (!isCommit(evt)) return
 
     const ops = await getOpsByType(evt)
-
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
-
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        // #happyhardcore posts only
+        return create.record.text.toLowerCase().includes('#happyhardcore')
       })
       .map((create) => {
-        // map alf-related posts to a db row
+        // map posts to a db row
         return {
           uri: create.uri,
           cid: create.cid,
